ed06aeb43f217941309bb71d303f9d97456c5edf
Carmine Di Gruttola <carmine.di.gruttola@pwc.com>
2022-08-17 15:37:14 +0200

    add sh to create zip module

---
+++ zip-for-marketplace.sh
@@ -0,0 +1,17 @@
+rm -rf tmp
+mkdir -p tmp/einvoice
+cp -R classes tmp/einvoice
+cp -R config tmp/einvoice
+cp -R override tmp/einvoice
+cp -R sql tmp/einvoice
+cp -R src tmp/einvoice
+cp -R translations tmp/einvoice
+cp -R views tmp/einvoice
+cp -R upgrade tmp/einvoice
+cp -R vendor tmp/einvoice
+cp -R index.php tmp/einvoice
+cp -R logo.png tmp/einvoice
+cp -R einvoice.php tmp/einvoice
+cp -R config.xml tmp/einvoice
+cd tmp && find . -name ".DS_Store" -delete
+zip -r einvoice.zip . -x ".*" -x "__MACOSX"
